e072d6acd184c1d65909018874d50d75372a9f5d
Matheus Alves <theuves@gmail.com>
2020-02-17 21:47:40 -0400

    adiciona método setWord

--- src/Noun.js
+++ src/Noun.js
@@ -1,32 +1,42 @@
 'use strict';
 
+const Pluralizer = require('./Pluralizador');
+
 class Noun {
     constructor() {
-        this.pluralizer = new Pluralizer();
+        this.word = ''
         this.count = 1;
+        this.pluralizer = new Pluralizer();
+    }
+
+    /**
+     * Definir palavra.
+     */
+    setWord(value) {
+        this.word = value;
     }
 
     /**
      * Definir quantidade.
      */
     setCount(value) {
-      this.count = value;
+        this.count = value;
     }
 
     /**
      * Definir pluralizador com as respectivas regras.
      */
     setPluralizer(pluralizer) {
-      this.pluralizer = pluralizer;
+        this.pluralizer = pluralizer;
     }
 
     /**
      * Pluralizar palavra.
      */
     pluralize(word) {
-      if (!word || typeof word !== 'string') return word;
+        if (!word || typeof word !== 'string') return word;
 
-      return this.pluralizer.pluralizeWord(word, this.count);
+        return this.pluralizer.pluralizeWord(this.word, this.count);
     }
 }
 
